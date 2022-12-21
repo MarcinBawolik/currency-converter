c81008d12779db988783da671a7fb984fffe918e
Marcin Bawolik <marcinbawolik2000@gmail.com>
2022-12-21 13:06:42 +0100

    js function first changes

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-let amount = document.querySelector(".js-form__amount")
-let input = document.querySelector(".js-input")
-let output = document.querySelector(".js-output")
-let button = document.querySelector(".js-form__button")
-let result = document.querySelector(".js-resultElement")
-let form = document.querySelector(".js-form")
+{
+    const amount = document.querySelector(".js-form__amount")
+    const input = document.querySelector(".js-input")
+    const output = document.querySelector(".js-output")
+    const result = document.querySelector(".js-resultElement")
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const upDateResultText = (amount, rateresult, input, output) => {
+        result.innerText = `${amount.value}${input.value}
+    =${rateresult.toFixed(2)}${output.value}`
 
-    let inputoutput = `${input.value}/${output.value}`;
-
-    let rate;
-    switch (inputoutput) {
-        case "PLN/USD":
-            rate = 0,22;
-            break;
-        case "PLN/EUR":
-            rate = 0,21;
-            break;
-        case "USD/EUR":
-            rate = 0,95;
-            break;
-        case "USD/PLN":
-            rate = 4.4351;
-            break;
-        case "EUR/USD":
-            rate = 1,05;
-            break;
-        case "EUR/PLN":
-            rate = 4,6821;
-        default:
-            rate = 1;
     }
-    let amountvalue = amount.value;
-    let rateresult = amountvalue * rate
-    
-    
-result.innerText = `${amount.value}${input.value}
-=${rateresult.toFixed(2)}${output.value}`
 
-});
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const inputoutput = `${input.value}/${output.value}`;
+
+        let rate;
+        switch (inputoutput) {
+            case "PLN/USD":
+                rate = 0, 22;
+                break;
+            case "PLN/EUR":
+                rate = 0, 21;
+                break;
+            case "USD/EUR":
+                rate = 0, 95;
+                break;
+            case "USD/PLN":
+                rate = 4.4351;
+                break;
+            case "EUR/USD":
+                rate = 1, 05;
+                break;
+            case "EUR/PLN":
+                rate = 4, 6821;
+            default:
+                rate = 1;
+        }
+        const amountvalue = amount.value;
+        const rateresult = amountvalue * rate
+
+
+
+
+        upDateResultText(amount, rateresult, input, output)
+    };
+
+
+
+    const init = () => {
+        const form = document.querySelector(".js-form")
+
+        form.addEventListener("submit", onFormSubmit);
+    }
+    init()
+
+}
