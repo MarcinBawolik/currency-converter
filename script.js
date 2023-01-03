33b3b547ea5b56fbde9ca4b198c7e221bbc6a449
Marcin Bawolik <marcinbawolik2000@gmail.com>
2023-01-03 10:59:52 +0100

    changed function names

--- script.js
+++ script.js
@@ -1,51 +1,46 @@
 {
-    const amount = document.querySelector(".js-form__amount")
-    const input = document.querySelector(".js-input")
-    const output = document.querySelector(".js-output")
-    
-const calculateResult = (currencyPair) => {
-    switch (currencyPair) {
-        case "PLN/USD":
-            return 0.22;               
-        case "PLN/EUR":
-          return 0.21;               
-        case "USD/EUR":
-          return 0.95;              
-        case "USD/PLN":
-          return 4.4351;              
-        case "EUR/USD":
-          return 1.05;            
-        case "EUR/PLN":
-          return 4.6821;
-        default:
-          return 1;
-    }
-}
-    
-    const updateResultText = (amount, result, input, output) => {
-        const resultElement = document.querySelector(".js-resultElement")
-        resultElement.innerText = `${amount.value}${input.value}
-    =${result.toFixed(2)}${output.value}`
 
+    const calculateResult = (currencyPair) => {
+        switch (currencyPair) {
+            case "PLN/USD":
+                return 0.22;
+            case "PLN/EUR":
+                return 0.21;
+            case "USD/EUR":
+                return 0.95;
+            case "USD/PLN":
+                return 4.4351;
+            case "EUR/USD":
+                return 1.05;
+            case "EUR/PLN":
+                return 4.6821;
+            default:
+                return 1;
+        }
+    }
+
+    const updateResultText = (amountElement, result, inputCurrencyElement, outputCurrencyElement) => {
+        
+        
+        const resultElement = document.querySelector(".js-resultElement")
+        resultElement.innerText = `${amountElement.value}${inputCurrencyElement.value}
+    =${result.toFixed(2)}${outputCurrencyElement.value}`
     }
 
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const currencyPair = `${input.value}/${output.value}`;
+        const amountElement = document.querySelector(".js-form__amount")
+        const inputCurrencyElement = document.querySelector(".js-input")
+        const outputCurrencyElement = document.querySelector(".js-output")
 
+        const currencyPair = `${inputCurrencyElement.value}/${outputCurrencyElement.value}`;
         const rate = calculateResult(currencyPair)
-        
-        const amountvalue = amount.value;
-        const result = amountvalue * rate
+        const amountValue = +amountElement.value;
+        const result = amountValue * rate
 
-
-
-
-        updateResultText(amount, result, input, output)
+        updateResultText(amountElement, result, inputCurrencyElement, outputCurrencyElement)
     };
-
-
 
     const init = () => {
         const form = document.querySelector(".js-form")
